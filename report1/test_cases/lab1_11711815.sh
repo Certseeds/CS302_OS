5de63870a81b5bad075da0639dad757835f83e43
Certseeds <51754303+Certseeds@users.noreply.github.com>
2020-02-21 09:28:09 +0800

    update shell, use queue.
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- report1/test_cases/lab1_11711815.sh
+++ report1/test_cases/lab1_11711815.sh
@@ -5,13 +5,17 @@ set -eoux pipefail
  # @Organization: SUSTech
  # @Author: nanoseeds
  # @Date: 2020-02-19 19:09:27
- # @LastEditors: Please set LastEditors
- # @LastEditTime: 2020-02-20 23:00:10
+ # @LastEditors: nanoseeds
+ # @LastEditTime: 2020-02-21 09:27:41
  ###
-
+# TODO 递归改成循环,变成栈
 public_str1=""
 file_number=0
 folder_number=0
+begin=0
+end=0
+output=""
+path_array=()
 rela_absu(){
     if [[ -z $1 ]]; then
         return 1
@@ -42,65 +46,68 @@ rela_absu(){
     public_str1=${tmp_fullpath1}/${temp_path2}
     return 0
 }
-
+get_out_paths(){
+    local origin_path=`pwd`
+    local path=${1}
+    echo ${path}
+    cd ${path}
+    if [[ "" != `ls ${path}` ]]; then
+        for file in * ; do
+            echo ${file}
+            file_number=$(( file_number + 1 ))
+            if [[ -d ${file} ]]; then
+                rela_absu ${file}
+                path_array[${end}]="${public_str1}"
+                end=$(( end + 1 ))
+                file_number=$(( file_number - 1 ))
+                folder_number=$(( folder_number + 1 ))
+            fi 
+        done
+        cd ${origin_path}
+    fi
+}
+out_paths (){
+    local origin_path=`pwd`
+    local path=${1}
+    local path_m1=${path:0:1}
+    echo ${path}
+    cd ${path}
+    local inside_path=`pwd`
+    echo  "["${inside_path##*/}"]" >> ${output}
+    if [[ "" != `ls ${path}` ]]; then
+        for file in * ; do
+            echo ${file}
+            rela_absu ${file}
+            echo "will insert ${public_str1}"
+            if [[ -d ${file} ]]; then
+                local length=`expr ${#public_str1} - 1`
+                echo ${public_str1:0:${length}} >> ${output}
+            else 
+                echo ${public_str1} >> ${output}
+            fi 
+        done
+        cd ${origin_path}
+    fi
+    echo "" >> ${output}
+}
 main(){
     echo "main begin"
-    echo "[Directories Count]:${file_number}" 
-    echo "[Files Count]:${folder_number}"
-    local readpath_1=${1}
-    local readpath_2=${2}
-    echo ${readpath_1} ${readpath_2}
-    local readpath_3=${readpath_1:0:-1}
-    local readpath_4=${readpath_2:0:-1}
-    echo  "["${readpath_3##*/}"]" >> ${readpath_2}    
-    local path_ori=`pwd`
-    cd ${readpath_1}
-    local count_value=0
-    local before=0
-    if [[ "" = `ls ${readpath_1}` ]]; then
-        echo "" >> ${readpath_2}
-        before=1
-    fi
-    if [[ ${before} == 0 ]]; then
-    for file in *
+    readpath_1=${1}
+    readpath_3=${readpath_1:0:-1}
+    echo "${readpath_1}" "${output}"
+    get_out_paths ${readpath_1}
+    out_paths ${readpath_1}
+    echo ${path_array[@]}
+    echo ${end}
+    while (( ${begin} < ${end} )) 
     do
-        echo "'${file}'"
-        local array[count_value]="${file}"
-        count_value=$(( count_value + 1 ))
+        get_out_paths ${path_array[begin]}
+        out_paths ${path_array[begin]}
+        begin=$(( ${begin} + 1 ))
     done
-    echo ${array[*]}
-    local array=($(for l in ${array[*]}; do echo ${l}; done | sort))
-    echo ${array[*]}
-    echo "read into finish"
-    echo ${array[*]}
-    for file in ${array[*]}; do
-        rela_absu ${file}
-        temp=${public_str1}
-        local length=`expr ${#temp}`
-        file_number=$(( file_number + 1 ))
-        if [[ -d ${temp} ]]; then
-            length=`expr ${length} - 1`
-            file_number=$(( file_number - 1 ))
-            folder_number=$(( folder_number + 1 ))
-        fi
-        echo ${temp:0:${length}}
-        echo ${temp:0:${length}} >> ${readpath_2}
-    done
-    echo "" >> ${readpath_2}
-    echo "output finish "
-    for file in ${array[*]}; do
-        if [[ -d ${file} ]]; then
-            rela_absu ${file}
-            local temp=${public_str1}
-            echo ${temp} ${readpath_2}
-            main ${temp} ${readpath_2}
-        fi
-    done
-    fi
-    echo "rec finish"
-    cd ${path_ori}
+    echo ${path_array[@]}
 }
-## 逻辑大概是,读进来之后,都先转换成绝对路径,先都输出一边,然后吧目录加进列表中,递归调用.
+## 逻辑大概是,读进来之后,都先转换成绝对路径,先都输出一边,然后吧目录加进列表中,循环调用
 
 OLD_IFS=$IFS
 IFS=$(echo -en "\n\b")
@@ -116,8 +123,8 @@ if [[ -f ${output} ]]; then
     rm ${output}
 fi
 touch ${output}
-main ${input} ${output}
-echo "" >> ${output}
+main ${input}
+echo "main finish " 
 echo "[Directories Count]:${folder_number}" >> ${output}
 echo "[Files Count]:${file_number}" >> ${output}
 cat ${output}
